24261d15134eacdf77a5b6e1d0ebb2de2d2a919b
George <cd5160866@126.com>
2023-08-05 23:55:46 +0800

    [ADD]add banner model

---
+++ app/src/main/java/com/example/newdemo/model/BannerModel.kt
@@ -0,0 +1,12 @@
+package com.example.newdemo.model
+
+data class BannerModel(
+    val desc: String,
+    val id: Int,
+    val imagePath: String,
+    val isVisible: Int,
+    val order: Int,
+    val title: String,
+    val type: Int,
+    val url: String
+)
